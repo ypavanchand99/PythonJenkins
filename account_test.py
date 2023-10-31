f99795fbb70cc6da56d72ddfe546973288bb3933
ypavanchand99 <48638309+ypavanchand99@users.noreply.github.com>
2023-10-31 13:37:29 +0530

    Update account_test.py

--- account_test.py
+++ account_test.py
@@ -5,7 +5,7 @@ class Test(unittest.TestCase):
     accInfo = AccountClass.account()
 
     def test_check_password_length(self):
-        print("Checking possible passwords\n")
+        print("Checking possible passwords..\n")
         passwordList = [ 'abeautifulday', 'astrictboss', 'alovelyhouse' ]
 
         for passwd in passwordList:
